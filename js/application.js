f92bc0777c20270ec444eea4384dbc18d45cb401
Lsong <song940@gmail.com>
2020-07-01 11:24:19 +0800

    remove posts

--- js/application.js
+++ js/application.js
@@ -3,6 +3,5 @@ import { render as renderPosts } from './posts.js';
 import { render as renderProjects } from './projects.js';
 
 renderNavbar('#navbar');
-renderPosts('#posts ul');
 renderProjects('#projects ul');
 
