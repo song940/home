489acedb6a459baa13fe2610e13c0a786624af55
Lsong <song940@gmail.com>
2021-01-25 21:22:46 +0800

    add link

--- js/application.js
+++ js/application.js
@@ -2,6 +2,5 @@ import { render as renderNavbar } from './navbar.js';
 import { render as renderPosts } from './posts.js';
 import { render as renderProjects } from './projects.js';
 
-renderNavbar('#navbar');
 renderProjects('#projects ul');
 
